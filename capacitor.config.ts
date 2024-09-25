2dd429196a302127ea14e311aa38fcdc8e1cb81c
Will Carvalho <william.melise@gmail.com>
2024-09-25 17:40:55 -0300

    build: configure project to run in xcode and change appId to io.ionic.helloworld

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.helloworld',
   appName: 'ionic-hello-world',
   webDir: 'www'
 };
