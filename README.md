8a990bfd7edfb02b2b2196f59d6cb6708eaa8f2b
Will Carvalho <william.melise@gmail.com>
2024-09-30 21:21:06 -0300

    feat: implement push notification

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ionic-hello-world
+
+## Capacitor Android Documentation
+
+- https://capacitorjs.com/docs/android
+
+## Capacitor iOS Documentation
+
+- https://capacitorjs.com/docs/ios
+
+## Firebase Apple Open Source Development
+
+- https://github.com/firebase/firebase-ios-sdk
+- https://capacitorjs.com/docs/guides/push-notifications-firebase#ios
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PushNotifications } from '@capacitor/push-notifications';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,40 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() {
+    this.initPushNotifications();
+  }
+
+  initPushNotifications() {
+    // Solicitar permissão para enviar notificações
+    PushNotifications.requestPermissions().then(permission => {
+      if (permission.receive === 'granted') {
+        // Registrar o dispositivo para receber notificações
+        PushNotifications.register();
+      }
+    });
+
+    // Listener quando o dispositivo é registrado com sucesso
+    PushNotifications.addListener('registration', token => {
+      console.log('Token de registro: ', token.value);
+      // Aqui você pode enviar o token para o seu servidor ou Firebase
+    });
+
+    // Listener para quando houver erro no registro
+    PushNotifications.addListener('registrationError', error => {
+      console.error('Erro de registro: ', error);
+    });
+
+    // Listener para quando uma notificação é recebida
+    PushNotifications.addListener('pushNotificationReceived', notification => {
+      console.log('Notificação recebida: ', notification);
+      // Aqui você pode lidar com a notificação
+    });
+
+    // Listener para quando o usuário clica na notificação
+    PushNotifications.addListener('pushNotificationActionPerformed', notification => {
+      console.log('Ação da notificação: ', notification);
+      // Aqui você pode executar a ação da notificação
+    });
+  }
 }
