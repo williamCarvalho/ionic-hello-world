36f2b72e728181bf939e81fc5ec680d25fa35060
Will Carvalho <william.melise@gmail.com>
2024-09-30 21:17:56 -0300

    build: configure project to support firebase push notifications

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.helloworld',
   appName: 'ionic-hello-world',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"]
+    }
+  }
 };
 
 export default config;
